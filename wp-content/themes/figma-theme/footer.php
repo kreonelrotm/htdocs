5738041fc1ba9d03b33fe1de13b99d914b3e87db
kreonelrotm <cernicamihaidaniel@gmail.com>
2024-05-09 00:11:31 +0300

    FIGMA-PROJECT v2

--- wp-content/themes/figma-theme/footer.php
+++ wp-content/themes/figma-theme/footer.php
@@ -1,28 +1,11 @@
 <?php wp_footer(); ?>
-<div class="row justify-content-center">
-<div class="col-lg-12 col-md-12 col-sm-9 position-relative">
-        <div class="footer-section">
-            <div class="footer-top">
-                <div class="footer-left">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" alt="Logo">
-
-                </div>
-                <div class="footer-right">
-                </div>
-            </div>
-            <hr>
-            <div class="footer-bottom">
-                <div class="footer-social">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.svg" alt="Facebook">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.svg" alt="X">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/linkedin.svg" alt="Linkedin">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.svg" alt="Youtube">
-                </div>
-                <p>© CaseGuard Web Dev Assessment 2024</p>
-            </div>
-        </div>
+<footer>
+    <div class="footer-left">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" alt="Logo">
     </div>
-</div>
+
+    <p>© CaseGuard Web Dev Assessment 2024</p>
+</footer>
 </body>
 
 </html>
--- wp-content/themes/figma-theme/includes/section-article.php
+++ wp-content/themes/figma-theme/includes/section-article.php
@@ -1,27 +1,23 @@
 <?php
-// Verificăm dacă există un articol curent înainte de a încerca să afișăm conținutul
 if (have_posts()):
     while (have_posts()):
         the_post(); ?>
-        <div class="row justify-content-center" id="section2">
-            <div class="col-lg-9 col-md-9 col-sm-12 position-relative">
-                <div class="content-article">
-                    <div class="title">
-                        <h1>
-                            <span><?php the_title(); ?></span>
-                        </h1>
-                    </div>
-                    <?php if (has_post_thumbnail()): ?>
-                        <div class="thumbnail">
-                            <?php
-                            // Obținem imaginea thumbnail cu dimensiunea specificată
-                            the_post_thumbnail('large', array('class' => 'img-fluid')); 
-                            ?>
-                        </div>
-                    <?php endif; ?>
-                    <p><?php the_content(); ?></p>
-                    <div class="tags"><?php the_tags('Tags: ', ', ', '<br />'); ?></div>
+        <div id="section2">
+            <div class="content-article">
+                <div class="title">
+                    <h1>
+                        <span><?php the_title(); ?></span>
+                    </h1>
                 </div>
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="thumbnail">
+                        <?php
+                        the_post_thumbnail('large', array('class' => 'img-fluid'));
+                        ?>
+                    </div>
+                <?php endif; ?>
+                <p><?php the_content(); ?></p>
+                <div class="tags"><?php the_tags('Tags: ', ', ', '<br />'); ?></div>
             </div>
         </div>
     <?php endwhile;
